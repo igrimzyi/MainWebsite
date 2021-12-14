f8c817292b376292b64ae3745d9c2232b830994f
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-14 00:20:35 -0800

    Refactoring

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+
+
+app.engine('ejs', ejsMate);
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine' , 'ejs');
+
+app.get('/', (req,res)=>{
+    res.render('tabs/mainPage')
+})
 
 
 app.listen(3000 , ()=>{
